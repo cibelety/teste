3b638573bbb5ac48f0546705ec9ae26bb426fbc8
Cibele Takaoka Yamamoto <cibelety@gmail.com>
2017-12-30 19:47:27 -0200

    Meu primeiro commit

---
+++ app/src/main/java/br/com/educacaodesurdos/learnjava/HelloWorld.java
@@ -0,0 +1,22 @@
+package br.com.educacaodesurdos.learnjava;
+
+import android.app.admin.SystemUpdatePolicy;
+
+/**
+ * Created by cibelety on 27/12/17.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My first Java program");
+
+        String cibele = "Cibele Yamamoto";
+        System.out.println(cibele);
+
+        int cibeleSalary =33;
+        int monthly = cibeleSalary * 4;
+        System.out.println(cibeleSalary);
+        System.out.println(monthly);
+    }
+}
